f26e310f2d0d231b10f6cb0a325eaafe98907970
Anowar Islam <anowar.islam@cloudknox.io>
2021-03-07 17:50:55 -0800

    fix CloudKnox API struct

--- aws-cloudknox-config/lambda/CloudKnox_IAMRightsize.py
+++ aws-cloudknox-config/lambda/CloudKnox_IAMRightsize.py
@@ -3,7 +3,7 @@
 
 #  Lambda for IAM Rightsizing with CloudKnox and AWS Config
 #  - Creates new IAM Policy and detaches existing policies for IAM user
-#  - New IAM Policy created based on policy data retrieved from CloudKnox JEP Controller 
+#  - New IAM Policy created based on policy data retrieved from CloudKnox JEP Controller
 
 
 # @kmmahaj
@@ -138,14 +138,7 @@ def get_cloudknox_remediation_policies(access_token, user_arn, ck_config=None):
         'identityIds': [user_arn],
         'aggregation': {'type': 'SUMMARY'},
         'requestParams': {'scope': None, 'resource': None, 'resources': None, 'condition': None},
-        'filter': {
-            'historyDays': 90,
-            'preserveReads': True,
-            'historyDuration': {
-                'startTime': curr_time,
-                'endTime': curr_time - (90 * 86400 * 1000)
-            }
-        }
+        'filter': {'historyDays': 90}
     }
     payload = json.dumps(cloudknox_dict)
     logger.info(f'remediation policy request payload {payload}')
@@ -186,7 +179,7 @@ def get_remediated_policy_list(user_name, ck_remediated_policies=None):
     iam_policies = {}
     count = 1
     for ck_policy in ck_remediated_policies:
-        iam_policy_doc = json.dumps(ck_policy)
+        iam_policy_doc = json.dumps(ck_policy['policy'])
         policy_name = '-'.join(['cloudknox', 'remediated', 'policy', user_name, str(curr_time), str(count)])
         logger.info(f'policy {policy_name} with document {iam_policy_doc}')
         count += 1
@@ -207,12 +200,12 @@ def attach_remediated_iam_policies(client, iam_user, iam_policies=None):
             PolicyName=policy_name,
             PolicyDocument=policy_doc
         )
-        logger.info(f'create_policy for {policy_name} response {json.dumps(create_policy_resp)}')
+        logger.info(f'create_policy for {policy_name} response {create_policy_resp}')
         attach_user_policy_resp = client.attach_user_policy(
             UserName=iam_user,
             PolicyArn=create_policy_resp['Policy']['Arn']
         )
-        logger.info(f'attach_user_policy for {policy_name} response {json.dumps(attach_user_policy_resp)}')
+        logger.info(f'attach_user_policy for {policy_name} response {attach_user_policy_resp}')
         time.sleep(.1)
 
 
@@ -224,31 +217,33 @@ def clean_unused_iam_policies(client, iam_user, remediated_iam_policy_list=None)
     :param remediated_iam_policy_list: list of required policy names
     """
     list_attached_user_policies_resp = client.list_attached_user_policies(UserName=iam_user)
+    logger.info(f'list_attached_user_policies_resp {list_attached_user_policies_resp}')
     if len(list_attached_user_policies_resp['AttachedPolicies']) > 0:
         for policy in list_attached_user_policies_resp['AttachedPolicies']:
             policy_arn = policy['PolicyArn']
             if policy['PolicyName'] not in remediated_iam_policy_list:
                 logger.info(f'policy {policy_arn} to be detached')
                 detach_user_policy_resp = client.detach_user_policy(UserName=iam_user, PolicyArn=policy_arn)
-                logger.info(f'detach_user_policy for {policy_arn} response {json.dumps(detach_user_policy_resp)}')
+                logger.info(f'detach_user_policy for {policy_arn} response {detach_user_policy_resp}')
             else:
                 logger.info(f'policy {policy_arn} skipped')
 
     list_groups_for_user_resp = client.list_groups_for_user(UserName=iam_user)
+    logger.info(f'list_groups_for_user_resp {list_groups_for_user_resp}')
     if len(list_groups_for_user_resp['Groups']) > 0:
         for group in list_groups_for_user_resp['Groups']:
             group_name = group['GroupName']
             logger.info(f'group {group_name} to be detached')
             remove_user_from_group_resp = client.remove_user_from_group(GroupName=group_name, UserName=iam_user)
-            logger.info(f'remove_user_from_group for {group_name} response {json.dumps(remove_user_from_group_resp)}')
+            logger.info(f'remove_user_from_group for {group_name} response {remove_user_from_group_resp}')
 
     list_user_policies_resp = client.list_user_policies(UserName=iam_user)
-    if len(list_user_policies_resp['Policies']) > 0:
-        for policy in list_user_policies_resp['Policies']:
-            policy_arn = policy['PolicyArn']
-            logger.info(f'user inline policy {policy_arn} to be deleted')
-            delete_user_policy_resp = client.delete_user_policy(UserName=iam_user, PolicyArn=policy_arn)
-            logger.info(f'delete_user_policy for {policy_arn} response {json.dumps(delete_user_policy_resp)}')
+    logger.info(f'list_user_policies_resp {list_user_policies_resp}')
+    if len(list_user_policies_resp['PolicyNames']) > 0:
+        for policy in list_user_policies_resp['PolicyNames']:
+            logger.info(f'user inline policy {policy} to be deleted')
+            delete_user_policy_resp = client.delete_user_policy(UserName=iam_user, PolicyName=policy)
+            logger.info(f'delete_user_policy for {policy} response {delete_user_policy_resp}')
 
 
 def lambda_handler(event, context):
@@ -277,4 +272,3 @@ def lambda_handler(event, context):
     iam_client = session.client(service_name='iam')
     attach_remediated_iam_policies(iam_client, iam_user, iam_policies)
     clean_unused_iam_policies(iam_client, iam_user, iam_policies.keys())
-
